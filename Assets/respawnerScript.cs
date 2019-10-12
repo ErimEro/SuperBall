56def0ad15f803e31530e9eb16df7a1eedd9bc93
ErimEro <erogiking@gmail.com>
2019-10-12 10:52:12 +0300

    Top Hakkı Eklendi

--- Assets/respawnerScript.cs
+++ Assets/respawnerScript.cs
@@ -1,15 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class respawnerScript : MonoBehaviour {
 
+public int tophakki=5;
 public GameObject top;
 public float waitTime=2;
 public int topSayaci=0;
 	// Use this for initialization
 	void Start () {
-		
+		GameObject.Find("TopHakkiUI").GetComponent<Text>().text=tophakki.ToString();
 	}
 	public void StartRespawnCounter(){
 		StartCoroutine(RespawnTop());
@@ -19,6 +20,9 @@ public int topSayaci=0;
 		topSayaci++;
 		GameObject top1 = Instantiate(top,transform,true);
 		top1.GetComponent<topcontrol>().topNumarası= topSayaci;
+		tophakki--;
+		GameObject.Find("TopHakkiUI").GetComponent<Text>().text=tophakki.ToString();
+		
 	}
 	// Update is called once per frame
 	void Update () {
